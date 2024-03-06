584b809cf53f9c5bf45a71aa75e9987bd9e10e84
Saunak <76943154+Saunakghosh10@users.noreply.github.com>
2024-03-06 16:04:40 +0530

    changes

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,59 +1,64 @@
 // components/HomePage.js
-import React, { useState } from 'react';
-import { Link, Routes, Route, Outlet } from 'react-router-dom';
-import './HomePage.css';
-import logo from '../components/media/CodeCrafters.png';
+import React, { useState } from "react";
+import { Link, Routes, Route, Outlet } from "react-router-dom";
+import "./HomePage.css";
+import logo from "../components/media/CodeCrafters.png";
 
 const MayuriPage = () => <h2>Welcome to Mayuri!</h2>;
 const UnderbellyPage = () => <h2>Welcome to Underbelly!</h2>;
 
 const HomePage = () => {
-  const [selectedOption, setSelectedOption] = useState('mayuri');
+  const [selectedOption, setSelectedOption] = useState("mayuri");
   const [checked, setChecked] = useState(false);
+  const [toggle, setToggle] = useState(false);
 
   const handleToggle = () => {
     setChecked(!checked);
-    setSelectedOption(checked ? 'mayuri' : 'underbelly');
+    setSelectedOption(checked ? "mayuri" : "underbelly");
   };
 
   return (
     <div>
       <header>
         <div className="headerLine1">
-        <div className="logoDiv">
-          <img className='logo' src={logo} alt='logo'/>
-        </div>
-        <div className="toggle-slider">
-          <input
-            type="checkbox"
-            id="toggle"
-            checked={checked}
-            onChange={handleToggle}
-          />
-          <label htmlFor="toggle" className="slider" />
-        </div>
+          <div className="logoDiv">
+            <img className="logo" src={logo} alt="logo" />
+          </div>
+          <div className="toggle-slider">
+            <input
+              type="checkbox"
+              id="toggle"
+              checked={checked}
+              onChange={handleToggle}
+              className="checkbox"
+            />
+            <label htmlFor="toggle" className="slider">
+              <span className="mayuri">Mayuri</span>
+              <span className="underbelly">Underbelly</span>
+            </label>
+          </div>
         </div>
         <div className="headerLine2">
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to={`/${selectedOption}-menu`}>Menu</Link>
-            </li>
-            <li>
-              <Link to="/progress">Progress</Link>
-            </li>
-            <li>
-              <Link to="/contact">Contact</Link>
-            </li>
-          </ul>
-        </nav>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to={`/${selectedOption}-menu`}>Menu</Link>
+              </li>
+              <li>
+                <Link to="/progress">Progress</Link>
+              </li>
+              <li>
+                <Link to="/contact">Contact</Link>
+              </li>
+            </ul>
+          </nav>
         </div>
       </header>
       <main>
-        <h1>Welcome to {checked ? 'Underbelly' : 'Mayuri'}!</h1>
+        <h1>Welcome to {checked ? "Underbelly" : "Mayuri"}!</h1>
         <Routes>
           <Route path="/" element={<Outlet />} />
           <Route path="/mayuri-menu" element={<MayuriPage />} />
